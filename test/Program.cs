c4b777d31c92a39e3ea5b505163a7a3085850de5
unknown <masteruser@niz-unvetos.AFGN.local>
2023-03-30 16:02:01 +0300

    test1

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+List<int> numbers = new List<int>();
+numbers.Add(12);
+numbers.Add(5);
+numbers.Add(8);
+// numbers.Add(14);
+// numbers.Add(22);
+
+
+// numbers.AddRange(new int[] {3,4,5,6});
+
+
+// numbers.Clear();
+numbers.Insert(0,000);
+numbers.Insert(1,111);
+numbers.Insert(2,222);
+
+for (int i = 0; i < numbers.Count; i++)
+{
+
+    System.Console.WriteLine(numbers[i]);
+
+
+}
