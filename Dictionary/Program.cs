edf79ea11b127d2f4c606569eb814d426e4bd88c
unknown <masteruser@niz-unvetos.AFGN.local>
2023-03-31 14:48:44 +0300

    Dictionary ключ-значение

---
+++ Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+Dictionary<string, string> countrieCapitals = new Dictionary<string, string>();
+
+countrieCapitals.Add("Австралия","Канберра");
+countrieCapitals.Add("Турция","Анкара");
+countrieCapitals.Add("Швейцария","Берн");
+
+
+
+countrieCapitals.Remove("Турция");
+
+
+
+foreach (var key in countrieCapitals.Keys)
+{
+    System.Console.WriteLine($"Страна - {key}");
+}
