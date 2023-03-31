f1f5b32cfaa33f653499ac729dacb362b4277f01
unknown <masteruser@niz-unvetos.AFGN.local>
2023-03-31 08:58:50 +0300

    Q

---
+++ Q/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Queue<string> patients = new Queue<string>();
+
+patients.Enqueue("Василий");
+patients.Enqueue("Леша");
+patients.Enqueue("Рома");
+patients.Enqueue("Вова");
+
+System.Console.WriteLine("На прием идет " + patients.Dequeue());
+System.Console.WriteLine("Cледующий в очереди " + patients.Peek());
+
+foreach (var patien in patients)
+{
+    System.Console.WriteLine(patien);
+}
+
+
