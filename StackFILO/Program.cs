802ea38df2daf90a84ca10b155f39a3e2734aaaa
unknown <masteruser@niz-unvetos.AFGN.local>
2023-03-31 09:10:14 +0300

    Stack

---
+++ StackFILO/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+Stack<int> numbers = new Stack<int>();
+
+numbers.Push(1);
+numbers.Push(2);
+numbers.Push(3);
+numbers.Push(4);
+numbers.Push(5);
+
+// System.Console.WriteLine(numbers.Peek());
+
+
+// numbers.Pop();
+
+
+
+foreach (var number in numbers)
+{
+    System.Console.WriteLine(number);
+}
+
+
+while (numbers.Count > 0)
+{
+    System.Console.WriteLine("Следующее число в стеке " + numbers.Pop());
+}
