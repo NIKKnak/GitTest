d1369d15d64f11154e556f5170d20800ba55579b
unknown <masteruser@niz-unvetos.AFGN.local>
2023-04-04 08:29:05 +0300

    04.04.2023

---
+++ oop/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace CSharp
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            Car ferrari = new Car();
+            
+            // ferrari.
+
+
+
+        }
+    }
+
+    class Car
+    {
+        public string name;
+        private int horsePower;
+        public int age;
+        private int maxSpeed;
+
+
+    }
+
+
+}
