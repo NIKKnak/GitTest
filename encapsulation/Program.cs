498db12e77b204a2fe6fdde4b11be6aea33f3f7e
NIKnak <nik88350759@yandex.ru>
2023-04-12 15:29:47 +0300

    encapsulation

---
+++ encapsulation/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Renderer renderer = new Renderer();
+        Player player = new Player(55, 10);
+
+        renderer.Draw(player.X, player.Y);
+
+    }
+}
+
+
+class Renderer
+{
+    public void Draw(int x, int y, char character = '@')
+    {
+        Console.CursorVisible = false;
+        Console.SetCursorPosition(x, y);
+        System.Console.Write(character);
+        Console.ReadKey(true);
+
+
+    }
+}
+
+class Player
+{
+    private int _x;
+    private int _y;
+
+    // public int X
+    // {
+    //     get
+    //     {
+    //         return _x;
+    //     }
+
+    //     private set
+    //     {
+    //         //if (X > 0 && X < 100)
+    //         _x = value;
+    //     }
+    // }
+    public int X { get; private set; }
+    // запись Y = записи X
+    public int Y { get; private set; }
+
+    public Player(int x, int y)
+    {
+        //_x
+        X = x;
+        Y = y;
+        //_y
+    }
+
+
+
+}
+
+
+
